756157dc94a48f02a001bbb6785ffe813e9a50f1
Giannis Vlachopoulos <giannis.vlachopoulos13@gmail.com>
2021-06-11 10:07:42 -0700

    fixup

--- src/compact.ts
+++ src/compact.ts
@@ -2,27 +2,14 @@ function isObject(val) {
   return Object === val.constructor;
 }
 
-export function compact(
-  $,
-  ifCondition = [],
-  options: { dropValues?: any[], $?: any, $1?: any } = {},
-  keep = false
-) {
-  const { dropValues = [undefined], $: original, $1: auxiliary } = options;
+export function compact($, keysThatMustExist = [], dropValues: any[] = [undefined], keep = false) {
   if (Array.isArray($)) {
     $ = $.filter(o => dropValues.indexOf(o) === -1);
     if ($.length > 0) return $;
   } else if ($ && isObject($)) {
-    const keysThatMustExist = ifCondition.filter(a => typeof a === 'string');
     if (keysThatMustExist?.length > 0) {
       for (let key of keysThatMustExist) {
         if (dropValues.indexOf($[key]) !== -1) return;
-      }
-    }
-    const callbacks = ifCondition.filter(a => typeof a === 'function');
-    if (callbacks?.length > 0) {
-      for (let callback of callbacks) {
-        if (!callback(original, auxiliary)) return;
       }
     }
     const newObj = {};
